0cf05ec1afe095f80410274e46c1d51f7c105d76
KotaAono <aono@hnl.t.u-tokyo.ac.jp>
2022-04-20 00:51:55 +0900

    Delete unused functions

--- paramopt/utils.py
+++ paramopt/utils.py
@@ -1,30 +1,7 @@
 """ユーティリティ関数群."""
 
 from datetime import datetime
-import os
-from typing import Any, Dict
-import json
-
-
-def abspath(relpath: str, origin: str) -> str:
-    path_ = os.path.abspath(os.path.join(os.path.dirname(origin), relpath))
-    return path_
-
-
-def timestamp() -> str:
-    return datetime.now().strftime("%H:%M:%S.%f")
 
 
 def formatted_now() -> str:
     return datetime.now().strftime("%Y%m%d-%H%M%S")
-
-
-def read_json(filepath: str) -> Dict[str, Any]:
-    with open(filepath, 'r') as f:
-        data = json.load(f)
-    return data
-
-
-def write_json(data: Dict[str, Any], filepath: str) -> None:
-    with open(filepath, 'w') as f:
-        json.dump(data, f, indent=4)
