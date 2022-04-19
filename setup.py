38cd33da16ddecfeb86741b1131148b4407c1561
KotaAono <aono@hnl.t.u-tokyo.ac.jp>
2022-04-20 00:52:29 +0900

    Modify description

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="paramopt",
     version="0.1.0",
     license="MIT",
-    description="Measurement package for Keyence laser profilers",
-    author="KotaAono",
+    description="Library for easy parameter tuning using gaussian process regression",
+    author="Kota Aono",
     url="https://github.com/ut-hnl-lab/paramopt.git",
     packages=['paramopt'],
     install_requires=[
