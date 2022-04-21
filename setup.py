1011d55ea44d43703b1cee0a87ffd5909374dd0f
KotaAono <aono@hnl.t.u-tokyo.ac.jp>
2022-04-21 10:18:00 +0900

    Delete install_requires due to import errors in conda environment

--- setup.py
+++ setup.py
@@ -2,18 +2,10 @@ from setuptools import setup
 
 setup(
     name="paramopt",
-    version="0.1.0",
+    version="0.2.0",
     license="MIT",
     description="Library for easy parameter tuning using gaussian process regression",
     author="Kota Aono",
     url="https://github.com/ut-hnl-lab/paramopt.git",
     packages=['paramopt'],
-    install_requires=[
-        'matplotlib',
-        'numpy',
-        'GPy',
-        'GPyOpt',
-        'sklearn',
-        'scipy'
-    ]
 )
