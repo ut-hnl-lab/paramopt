3d44db46852eca973fdcc9610623c19252f3f279
KotaAono <aono@hnl.t.u-tokyo.ac.jp>
2022-07-07 21:10:54 +0900

    Update contents

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="paramopt",
     version="1.0.0",
     license="MIT",
-    description="Param Opt helps the researcher quickly and easily explore optimal experimental parameters",
+    description="Param Opt helps the researcher quickly and easily find best experimental parameters",
     author="blue-no",
     url="https://github.com/ut-hnl-lab/paramopt.git",
     packages=find_packages(),
