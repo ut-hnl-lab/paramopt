494187bccdaadc6d9ebcfa958fda97764c3bf29c
KotaAono <aono@hnl.t.u-tokyo.ac.jp>
2022-07-24 13:50:46 +0900

    Modify function visibility and add repr function

--- paramopt/utils/string.py
+++ paramopt/utils/string.py
@@ -1,12 +1,18 @@
 import os.path as osp
 from datetime import datetime
+from typing import Any
 
 
 def formatted_now() -> str:
     return datetime.now().strftime("%Y%m%d-%H%M%S")
 
 
-def _unique_path(path):
+def indent_repr(m: str, depth: int = 4, sep: str = "\n") -> str:
+    blank = " " * depth
+    return blank + m.replace(sep, "\n"+blank)
+
+
+def unique_path(path):
     ret = path
     stem, ext = osp.splitext(path)
     i = 1
